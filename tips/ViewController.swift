e230973bf5be6b207382e7dcea1d590f6beecca5
asianxjay <you@example.com>
2015-01-21 21:56:58 -0800

    tips exercise

---
+++ tips/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Jason Demetillo on 1/19/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChange(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    
+        
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+   
+    view.endEditing(true)
+   
+    }
+
+}
+
